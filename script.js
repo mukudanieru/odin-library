d9cf21227399418b97264c048a6a10ac31583b2a
mukudanieru <111953203+mukudanieru@users.noreply.github.com>
2025-10-22 10:59:56 +0800

    Add dynamic book card component - Created the book card component in HTML first - Display book information (title, author, pages, read status and component options) - Add book image placeholder with read/unread SVG indicators - Style book cards with in a certain way and add hover effects - Create newBookElement() function to generate book cards dynamically

--- script.js
+++ script.js
@@ -48,6 +48,162 @@ form.addEventListener("submit", (event) => {
     const newBook = new Book(id, title, author, pages, hasRead);
     newBook.log();
 
+    const library = document.querySelector("#library");
+    if (library.classList.contains("empty")) {
+        library.classList.remove("empty");
+        document.querySelector(".empty-list-title").remove();
+        document.querySelector(".empty-list-message").remove();
+    }
+
+    myLibrary.push(newBook);
+
+    myLibrary.forEach((currentBook) => {
+        library.appendChild(newBookElement(currentBook));
+    });
+
     form.reset();
     modal.classList.remove("active");
 });
+
+// Create a new book element
+function newBookElement(bookObject) {
+    const book = document.createElement("div");
+    book.classList.add("book");
+    book.id = bookObject.id;
+
+    const bookContent = document.createElement("div");
+    bookContent.classList.add("book-content");
+
+    // Adding Book Image
+    const bookImage = document.createElement("div");
+    bookImage.classList.add("book-image");
+
+    const readSVG = `
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            class="lucide lucide-book-check-icon lucide-book-check">
+            <path d="M4 19.5v-15A2.5 2.5 0 0 1 6.5 2H19a1 1 0 0 1 1 1v18a1 1 0 0 1-1 1H6.5a1 1 0 0 1 0-5H20"/>
+            <path d="m9 9.5 2 2 4-4" />
+        </svg>
+        <div class="status">Read</div>
+    `;
+
+    const unreadSVG = `
+        <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        class="lucide lucide-book-x-icon lucide-book-x">
+        <path d="m14.5 7-5 5" />
+        <path d="M4 19.5v-15A2.5 2.5 0 0 1 6.5 2H19a1 1 0 0 1 1 1v18a1 1 0 0 1-1 1H6.5a1 1 0 0 1 0-5H20"/>
+        <path d="m9.5 7 5 5" />
+    </svg>
+    <div class="status">Unread</div>
+    `;
+
+    if (bookObject.hasRead) {
+        bookImage.innerHTML = readSVG;
+        bookImage.classList.add("read");
+    } else {
+        bookImage.innerHTML = unreadSVG;
+        bookImage.classList.add("unread");
+    }
+
+    bookContent.appendChild(bookImage);
+
+    // Adding Book Information
+    const bookInformation = document.createElement("div");
+    bookInformation.classList.add("book-information");
+
+    const bookImportantInformation = document.createElement("div");
+    bookImportantInformation.classList.add("book-important-information");
+
+    const bookTitle = document.createElement("h1");
+    bookTitle.classList.add("book-title");
+    bookTitle.innerHTML = bookObject.title;
+    bookImportantInformation.appendChild(bookTitle);
+
+    const bookAuthor = document.createElement("h2");
+    bookAuthor.classList.add("book-author");
+    bookAuthor.innerHTML = bookObject.author;
+    bookImportantInformation.appendChild(bookAuthor);
+
+    bookInformation.appendChild(bookImportantInformation);
+
+    const bookPages = document.createElement("div");
+    bookPages.classList.add("book-pages");
+    bookPages.innerHTML = `${bookObject.pages} pages`;
+
+    bookInformation.appendChild(bookPages);
+
+    bookContent.appendChild(bookInformation);
+
+    // Adding Book Options
+    const bookOption = document.createElement("div");
+    bookOption.classList.add("book-option");
+
+    const readBtn = document.createElement("div");
+    readBtn.classList.add("book-btn");
+    readBtn.id = "read-btn";
+
+    const readBtnSVG = `
+        <svg
+            class="book-icon"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            class="lucide lucide-eye-icon lucide-eye">
+            <path d="M2.062 12.348a1 1 0 0 1 0-.696 10.75 10.75 0 0 1 19.876 0 1 1 0 0 1 0 .696 10.75 10.75 0 0 1-19.876 0"/>
+            <circle cx="12" cy="12" r="3" />
+        </svg>
+    `;
+
+    readBtn.innerHTML = readBtnSVG;
+    bookOption.appendChild(readBtn);
+
+    const removeBtn = document.createElement("div");
+    removeBtn.classList.add("book-btn");
+    removeBtn.id = "remove-btn";
+
+    const removeBtnSVG = `
+            <svg
+            class="book-icon"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            class="lucide lucide-x-icon lucide-x">
+            <path d="M18 6 6 18" />
+            <path d="m6 6 12 12" />
+        </svg>
+    `;
+
+    removeBtn.innerHTML = removeBtnSVG;
+    bookOption.appendChild(removeBtn);
+
+    book.appendChild(bookContent);
+    book.appendChild(bookOption);
+
+    return book;
+}
